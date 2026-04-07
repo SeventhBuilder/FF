f4fdf891c8277bb92a5d45668fc0c454730400c4
SeventhBuilder <125909441+SeventhBuilder@users.noreply.github.com>
2026-04-07 13:11:45 +0800

    Update init.lua

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JustApstl/FF/refs/heads/main/scripts/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SeventhBuilder/FF/refs/heads/main/scripts/main.lua"))()
